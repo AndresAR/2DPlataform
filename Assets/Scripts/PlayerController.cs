b819940e995da280253cbc3065c85fa393f00d05
Andrés Arias <Andres@MBP-de-Andres.home>
2016-09-20 19:15:30 -0300

    Modificacion Personaje y modificacion en animaciones

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,20 +5,29 @@ public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed;
 	public float jumpForce;
+	public bool grounded;
+	public LayerMask whatIsGround;
 
 	private Rigidbody2D myRigidbody;
+	private Collider2D myCollider;
 
 	// Use this for initialization
 	void Start () {
 		myRigidbody = GetComponent<Rigidbody2D> ();
+		myCollider = GetComponent<Collider2D> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+
+		grounded = Physics2D.IsTouchingLayers (myCollider, whatIsGround);
+
 		myRigidbody.velocity = new Vector2 (moveSpeed, myRigidbody.velocity.y);
 
 		if(Input.GetKeyDown(KeyCode.Space)){
-			myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);
+			if(grounded){
+				myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);	
+			}
 		}
 	}
 }
