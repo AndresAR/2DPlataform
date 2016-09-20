e107424852762ea9af01a98dcb0c43afcee86ce0
Andrés Arias <Andres@MBP-de-Andres.home>
2016-09-20 20:04:52 -0300

    Movimiento de camara y animaciones

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,24 +10,27 @@ public class PlayerController : MonoBehaviour {
 
 	private Rigidbody2D myRigidbody;
 	private Collider2D myCollider;
+	private Animator myAnimator;
 
 	// Use this for initialization
 	void Start () {
 		myRigidbody = GetComponent<Rigidbody2D> ();
 		myCollider = GetComponent<Collider2D> ();
+		myAnimator = GetComponent<Animator> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 
 		grounded = Physics2D.IsTouchingLayers (myCollider, whatIsGround);
-
 		myRigidbody.velocity = new Vector2 (moveSpeed, myRigidbody.velocity.y);
-
 		if(Input.GetKeyDown(KeyCode.Space)){
 			if(grounded){
 				myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);	
 			}
 		}
+
+		myAnimator.SetFloat ("Speed", myRigidbody.velocity.x);
+		myAnimator.SetBool ("Grounded", grounded);
 	}
 }
