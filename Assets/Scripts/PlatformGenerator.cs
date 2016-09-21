a7d55e1ce4943fe357a69305832e0cbf6c662b7b
Andrés Arias <Andres@MacBook-Pro-de-Andres.local>
2016-09-21 17:18:06 -0300

    Creación Random de tamaños de plataformas (3x1, 5x1, 7x1, 9x1)

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -3,32 +3,44 @@ using System.Collections;
 
 public class PlatformGenerator : MonoBehaviour {
 
+
 	public GameObject thePlatform;
+
 	public Transform generationPoint;
-	public ObjectPoller theObjectPool;
+
+	public ObjectPoller[] theObjectPools;
+
 	public float distanceBetween;
 	public float distanceBetweenMin;
 	public float distanceBetweenMax;
 
 	private float platformWidth;
+	private int platformSelector;
+	private float[] platformWidths;
 
 
 	// Use this for initialization
 	void Start () {
-		platformWidth = thePlatform.GetComponent<BoxCollider2D> ().size.x;
+		platformWidths = new float[theObjectPools.Length];
+		for(int i = 0; i < theObjectPools.Length; i++){
+			platformWidths [i] = theObjectPools [i].pooledObject.GetComponent<BoxCollider2D>().size.x;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (transform.position.x < generationPoint.position.x) {
 			distanceBetween = Random.Range (distanceBetweenMin, distanceBetweenMax);
-			transform.position = new Vector3 (transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+			platformSelector = Random.Range (0, theObjectPools.Length);
+			transform.position = new Vector3 (transform.position.x + (platformWidths[platformSelector] / 2) + distanceBetween, transform.position.y, transform.position.z);
 
-			//Instantiate (thePlatform, transform.position, transform.rotation);
-			GameObject newPlatform = theObjectPool.GetPooledObject();
+			//Instantiate (thePlatforms[platformSelector], transform.position, transform.rotation);
+			GameObject newPlatform = theObjectPools[platformSelector].GetPooledObject();
 			newPlatform.transform.position = transform.position;
 			newPlatform.transform.rotation = transform.rotation;
 			newPlatform.SetActive (true);
+
+			transform.position = new Vector3 (transform.position.x + (platformWidths[platformSelector] / 2) + distanceBetween, transform.position.y, transform.position.z);
 		}
 	}
 }
