40cc387b0037b182824a832846356d120aeb3ee9
Andrés Arias <Andres@MacBook-Pro-de-Andres.local>
2016-09-22 17:45:04 -0300

    Atura random de las plataformas, aumento del tiempo de salto para plataformas más altas

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -13,10 +13,17 @@ public class PlatformGenerator : MonoBehaviour {
 	public float distanceBetween;
 	public float distanceBetweenMin;
 	public float distanceBetweenMax;
+	public float maxHeightChange;
+	public Transform maxHeightPoint;
+
 
 	private float platformWidth;
 	private int platformSelector;
 	private float[] platformWidths;
+	private float minHeight;
+	private float maxHeight;
+	private float heighChange;
+
 
 
 	// Use this for initialization
@@ -25,6 +32,8 @@ public class PlatformGenerator : MonoBehaviour {
 		for(int i = 0; i < theObjectPools.Length; i++){
 			platformWidths [i] = theObjectPools [i].pooledObject.GetComponent<BoxCollider2D>().size.x;
 		}
+		minHeight = transform.position.y;
+		maxHeight = maxHeightPoint.position.y;
 	}
 	
 	// Update is called once per frame
@@ -32,7 +41,15 @@ public class PlatformGenerator : MonoBehaviour {
 		if (transform.position.x < generationPoint.position.x) {
 			distanceBetween = Random.Range (distanceBetweenMin, distanceBetweenMax);
 			platformSelector = Random.Range (0, theObjectPools.Length);
-			transform.position = new Vector3 (transform.position.x + (platformWidths[platformSelector] / 2) + distanceBetween, transform.position.y, transform.position.z);
+			heighChange = transform.position.y + Random.Range(maxHeightChange, -maxHeightChange);
+
+			if (heighChange > maxHeight) {
+				heighChange = maxHeight;
+			} else if (heighChange < minHeight) {
+				heighChange = minHeight;
+			}
+
+			transform.position = new Vector3 (transform.position.x + (platformWidths[platformSelector] / 2) + distanceBetween, heighChange, transform.position.z);
 
 			//Instantiate (thePlatforms[platformSelector], transform.position, transform.rotation);
 			GameObject newPlatform = theObjectPools[platformSelector].GetPooledObject();
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public bool grounded;
 	public float moveSpeed;
 	public float jumpForce;
-	public bool grounded;
+	public float jumpTime;
+
 	public LayerMask whatIsGround;
 
+	private float jumpTimeCounter;
 	private Rigidbody2D myRigidbody;
 	private Collider2D myCollider;
 	private Animator myAnimator;
@@ -17,6 +20,8 @@ public class PlayerController : MonoBehaviour {
 		myRigidbody = GetComponent<Rigidbody2D> ();
 		myCollider = GetComponent<Collider2D> ();
 		myAnimator = GetComponent<Animator> ();
+
+		jumpTimeCounter = jumpTime;
 	}
 
 	// Update is called once per frame
@@ -24,10 +29,26 @@ public class PlayerController : MonoBehaviour {
 
 		grounded = Physics2D.IsTouchingLayers (myCollider, whatIsGround);
 		myRigidbody.velocity = new Vector2 (moveSpeed, myRigidbody.velocity.y);
-		if(Input.GetKeyDown(KeyCode.Space)){
+
+		if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
 			if(grounded){
 				myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);	
 			}
+		}
+
+		if (Input.GetKey (KeyCode.Space) || Input.GetMouseButton (0)) {
+			if (jumpTimeCounter > 0) {
+				myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);	
+				jumpTimeCounter -= Time.deltaTime;
+			}
+		}
+
+		if(Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0)){
+			jumpTimeCounter = 0;
+		}
+
+		if(grounded){
+			jumpTimeCounter = jumpTime;
 		}
 
 		myAnimator.SetFloat ("Speed", myRigidbody.velocity.x);
